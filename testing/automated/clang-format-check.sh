9ed20cbc6eff53d3e57d0de27c07e0d76d75aef6
Hugo Brunie <hbrunie0@gmail.com>
2021-03-06 20:27:10 -0800

    Automating clang format from Asgard root.

--- testing/automated/clang-format-check.sh
+++ testing/automated/clang-format-check.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "running clang format. pwd: $(pwd)"
-
+mkdir -p patches/src/pde
+mkdir -p patches/src/device
 for file in $(find src -type f)
 do
   echo ${file}
-  diff ${file} <(clang-format ${file}) || exit
+  patch="./patches/${file}-format.patch"
+  diff ${file} <(clang-format ${file}) >> ${patch}
+  patch ${file} ${patch}
 done
