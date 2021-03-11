ce2e2887d1aae911f935533b693c1e8a7e5209d5
Hugo Brunie <hbrunie0@gmail.com>
2021-03-11 14:28:35 -0800

    Adding Shaman single precision (Sfloat).

--- src/tools.cpp
+++ src/tools.cpp
@@ -55,20 +55,46 @@ simple_timer timer;
 
 #ifdef ASGARD_USE_SHAMAN
 template<>
+std::string vec2csv(const std::vector<Sfloat>& vec)
+{
+    std::ostringstream s;
+    s << std::scientific;// << std::setprecision(16);
+
+  for(unsigned long i = 0; i < vec.size(); i++)
+  {
+      const Sfloat x = vec[i];
+      std::string dig =((int)x.digits()) < -1 ? "all (zero)" : std::to_string((int)x.digits());
+    s << '(' << i << ") "
+      << x << '|'
+      //<< x.number << '|'
+      //<< x.error  << '|'
+      << dig << std::endl;
+    // why not use Shaman's default streaming operator instead of the above?
+   //s << '(' << i << ") " <<x;
+    if (i+1 < vec.size())
+        {
+            s << ", ";
+        }
+  }
+  return s.str();
+}
+template<>
 std::string vec2csv(const std::vector<Sdouble>& vec)
 {
     std::ostringstream s;
-    s << std::scientific << std::setprecision(16);
+    s << std::scientific;// << std::setprecision(16);
 
   for(unsigned long i = 0; i < vec.size(); i++)
   {
       const Sdouble x = vec[i];
-    //s << '(' << i << ") "
-    //  << x.number << '|'
-    //  << x.error  << '|'
-    //  << x.digits();
+      std::string dig =((int)x.digits()) < -1 ? "all (zero)" : std::to_string((int)x.digits());
+    s << '(' << i << ") "
+      << x << '|'
+      //<< x.number << '|'
+      //<< x.error  << '|';
+      << dig << std::endl;
     // why not use Shaman's default streaming operator instead of the above?
-   s << '(' << i << ") " <<x;
+   //s << '(' << i << ") " <<x;
     if (i+1 < vec.size())
         {
             s << ", ";
